4718499fba53587a459088794b62617b027b5bd8
Rajssss <sssraj.sssraj@gmail.com>
2020-08-05 20:04:20 +0530

    Configure low level initializations.
    -> Priority grouping
    -> System exceptions
    -> Exception Priority

--- CubeHAL_USART_Example/Core/Src/msp.c
+++ CubeHAL_USART_Example/Core/Src/msp.c
@@ -8,14 +8,20 @@
 #include "stm32f4xx_hal.h"
 
 
-
+/*
+ * Low Level Initializations
+ * */
 void HAL_MspInit()
 {
-	//Low Level Initializations
-
 	//Setup Priority Grouping
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
-	//Enable Required exceptions
+	//Enable Required exceptions (here ex: USGFAULT, BUSFAULT, MEMFAULT)
+	SCB->SHCSR |= (0x7 << 16);
 
 	//Configure Priority of the exceptions
+	HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
+
 }
