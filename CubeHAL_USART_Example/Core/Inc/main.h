79e618a68c8429ace19ebf4c3f008b3d4e67851c
Rajssss <sssraj.sssraj@gmail.com>
2020-08-06 16:56:38 +0530

    Added USART High Level Initializations.
    ->Moved Function Prototypes to header.
    
    Signed-off-by: Rajssss <sssraj.sssraj@gmail.com>

--- CubeHAL_USART_Example/Core/Inc/main.h
+++ CubeHAL_USART_Example/Core/Inc/main.h
@@ -9,5 +9,12 @@
 #define INC_MAIN_H_
 
 
+//Prototypes
+void SystemClockConfig(void);
+void USART2_Init(void);
+void USART2_Error_Handler(void);
+
+
+
 
 #endif /* INC_MAIN_H_ */
--- CubeHAL_USART_Example/Core/Src/main.c
+++ CubeHAL_USART_Example/Core/Src/main.c
@@ -5,18 +5,20 @@
  *      Author: Raj.S
  */
 
-
+#include "main.h"
 #include "stm32f4xx_hal.h"
 
 
-void SystemClockConfig(void);
+UART_HandleTypeDef USART2_Handler;
 
 
 int main()
 {
 	HAL_Init();
+
 	SystemClockConfig();
 
+	USART2_Init();
 
 	return 0;
 }
@@ -29,5 +31,32 @@ void SystemClockConfig(void)
 
 }
 
+
+//Initialize USART2 in Async mode
+void USART2_Init(void)
+{
+	USART2_Handler.Instance = USART2;
+	USART2_Handler.Init.BaudRate = 115200;
+	USART2_Handler.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	USART2_Handler.Init.Mode = UART_MODE_TX_RX;
+	USART2_Handler.Init.Parity = UART_PARITY_NONE;
+	USART2_Handler.Init.StopBits = UART_STOPBITS_1;
+	USART2_Handler.Init.WordLength = UART_WORDLENGTH_8B;
+
+	if(HAL_UART_Init(&USART2_Handler) != HAL_OK)
+	{
+		//Something wrong
+		USART2_Error_Handler();
+	}
+
+}
+
+
+//USART Error handler
+void USART2_Error_Handler()
+{
+	while(1);
+
+}
 
 
